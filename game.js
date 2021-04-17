ec76ec086e60c7a1baac93f3b6e867db9f880978
Giyu <80783085+c1am@users.noreply.github.com>
2021-04-17 13:59:58 -0400

    created index.html and game.js files

---
+++ game.js
@@ -0,0 +1,32 @@
+start-button.addEventListener("click", startGame {
+
+}
+
+// add an event listener for click of the start button to start a "start-game" function
+    // When Clicking the Start button, change the display of timer-count and main board
+
+    // target the container with the timer
+        // start the countdown from 10 with a timer-start function
+
+    // target the main game
+        // reset the main game to all blanks
+
+// timer start function
+
+        // use setInterval to call the function
+                //check if the time is done
+                    // call "game over" function
+                // else
+                    // check if the word is complete
+                        // call "game won"
+                    // else deduct the time count
+
+// show blanks (game round)
+
+// add event listener for reset button to call the "reset-game" function
+
+// add keydown event listener to keydown document
+
+// For loop for each game
+
+    // change the score depending on win or loss
